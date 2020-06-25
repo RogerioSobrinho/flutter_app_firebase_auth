90db1a1fa924dea88db3f37d4098fc9a1d5f0475
rogerio.sobrinho <x216948>
2020-06-24 22:32:35 -0300

    feature: add new injection in locator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,5 +1,7 @@
 import 'package:example/scoped_models/home_view_model.dart';
-import 'package:example/services/shared_preferences_service.dart';
+import 'package:example/scoped_models/start_up_view_model.dart';
+import 'package:example/services/authentication_service.dart';
+import 'package:example/services/navigation_service.dart';
 import 'package:get_it/get_it.dart';
 import 'package:example/scoped_models/login_view_model.dart';
 import 'package:example/scoped_models/sign_up_view_model.dart';
@@ -8,9 +10,11 @@ GetIt locator = new GetIt();
 
 void setupLocator() {
   // Register services
-  locator.registerLazySingleton<SharedPrefService>(() => SharedPrefService());
+  locator.registerLazySingleton(() => NavigationService());
+  locator.registerLazySingleton(() => AuthentaticationService());
   // Register ScopedModels
   locator.registerFactory<LoginViewModel>(() => LoginViewModel());
   locator.registerFactory<SignUpViewModel>(() => SignUpViewModel());
   locator.registerFactory<HomeViewModel>(() => HomeViewModel());
+  locator.registerFactory<StartUpViewModel>(() => StartUpViewModel());
 }
